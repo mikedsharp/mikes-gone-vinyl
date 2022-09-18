c0c7e9efd3b07c8f03661f001378331d05db2bdf
Mike Sharp <msharp23@hotmail.co.uk>
2022-09-18 16:43:21 +0100

    feat: first pass at a grid of records with some fun little animations of the disc coming out of the sleeve

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,77 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
-  import Counter from "./lib/Counter.svelte";
+  import VinylRecord from "./lib/VinylRecord.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="banner">
+    <h1>Mike's record collection</h1>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="record-grid">
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
+    <VinylRecord />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank"
-      >SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style lang="scss">
-  @import "./random.scss";
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  @import url("./assets/fonts/fonts.scss");
 
-    &:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-
-    &.svelte:hover {
-      filter: drop-shadow(0 0 2em #3eff00aa);
-    }
+  :global(body) {
+    margin: 0;
   }
 
-  .read-the-docs {
-    color: #888;
+  :global(#app) {
+    background-image: url("./assets/textures/wood-background.jpg");
+    background-size: cover;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+  }
+
+  main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+  h1 {
+    font-family: "myUnderwood", Arial, Helvetica, sans-serif;
+    color: white;
+    color: #e64741;
+    text-shadow: 2px 2px #eee;
+    text-align: center;
+    font-size: 5rem;
+    flex: 1;
+    margin: 0;
+  }
+  .banner {
+    flex: 0;
+  }
+  .record-grid {
+    padding: 30px;
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    overflow: auto;
   }
 </style>
---
+++ src/lib/VinylRecord.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    let active = false;
+</script>
+
+<div
+    class={`vinyl-record ${active ? "vinyl-record--active" : ""}`}
+    on:mouseenter={() => {
+        active = true;
+    }}
+>
+    <div class="vinyl-record__sleeve" />
+    <div class="vinyl-record__disc" />
+</div>
+
+<style lang="scss">
+    .vinyl-record {
+        position: relative;
+        width: 512px;
+        height: 256px;
+        margin: 10px;
+        cursor: pointer;
+
+        &__sleeve {
+            position: absolute;
+            background-image: url("../assets//sleeves/pearl-jam-chicago-sleeve.png");
+            background-size: contain;
+            width: 256px;
+            height: 256px;
+            object-fit: scale-down;
+            z-index: 1;
+        }
+        &__disc {
+            position: absolute;
+            background-image: url("../assets//sleeves/pearl-jam-chicago-disc.png");
+            background-size: contain;
+            width: 256px;
+            height: 256px;
+            left: 0;
+            border-radius: 50%;
+            z-index: 0;
+        }
+        &:hover {
+            .vinyl-record__disc {
+                animation: record-slide-out 0.25s linear forwards;
+            }
+        }
+        &--active {
+            &:not(:hover) {
+                .vinyl-record__disc {
+                    animation: record-slide-in 0.25s linear forwards;
+                }
+            }
+        }
+    }
+
+    @keyframes record-slide-out {
+        from {
+            left: 0;
+        }
+        to {
+            left: 256px;
+        }
+    }
+    @keyframes record-slide-in {
+        from {
+            left: 256px;
+        }
+        to {
+            left: 0;
+        }
+    }
+</style>
